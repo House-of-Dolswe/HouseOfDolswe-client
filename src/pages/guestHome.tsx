240b3942444fd8cf1f58efafb3db9d72cb642d79
pkyujin <luxoris02@sookmyung.ac.kr>
2025-12-04 00:52:53 +0900

    feat: 카카오 버튼 수정 및 로그인 성공 후 페이지 이동

--- src/pages/guestHome.tsx
+++ src/pages/guestHome.tsx
@@ -1,6 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import LogoWhite from "../../public/logoWhite.svg";
-
+import KakaoIcon from "../../public/kakaoIcon.svg";
 
 const Container = styled.div `
   background: linear-gradient(#626262, #000000);
@@ -39,22 +40,40 @@ const LoginButton = styled.button`
   border-radius: 15px;
   position: absolute;
   bottom: 7vh;
+  background-color: #FEE500;
+  color: rgba(0, 0, 0, 0.85);
+  font-weight: 500;
+  display: flex;
+  justify-content: center;
+  gap: 2vw;
 `;
 
-const loginWithKakao = () => {
-  if (!window.Kakao || !window.Kakao.Auth) {
-    console.error("Kakao SDK not ready");
-    return;
-  }
+const KakaoImg = styled.img`
+  width: 4vw;
+  height: auto;
+`;
+
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  const loginWithKakao = () => {
+    if (!window.Kakao || !window.Kakao.Auth) return;
+    window.Kakao.Auth.setAccessToken(null);
 
   window.Kakao.Auth.login({
+    throughTalk: false,           // 카톡 자동 로그인 방지
+    persistAccessToken: false,    // 브라우저 저장 방지 -> 매번 팝업 창 뜸
     scope: "profile_nickname",
     success: (authObj: any) => {
       console.log("카카오 로그인 성공:", authObj);
       // access_token 여기서 바로 사용 가능
       window.Kakao.API.request({
         url: "/v2/user/me",
-        success: (res: any) => console.log(res),
+          success: (res: any) => {
+            console.log("유저 정보:", res);
+            navigate("/onboarding");
+          },
         fail: (err: any) => console.error(err),
       });
     },
@@ -63,11 +82,6 @@ const loginWithKakao = () => {
     },
   });
 };
-
-
-
-export default function Home() {
-
 
   return (
     <Container>
@@ -78,6 +92,7 @@ export default function Home() {
             <LogoText>언제 어디서든, 내 주머니 속 포켓 돌쇠</LogoText>
         </LogoWrapper>
       <LoginButton onClick={loginWithKakao}>
+        <KakaoImg src={KakaoIcon} alt="Kakao Icon" />
         카카오 로그인
       </LoginButton>
     </Container>
